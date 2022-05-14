f3b2c467b10b38a59962b3194301e27fa2d59357
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-05-14 13:04:09 +0200

    Log4j2 and AWS logging
    https://github.com/awspring/spring-cloud-aws/issues/382#issuecomment-1126673579

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,12 +4,24 @@ import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        // run with env variable AWS_ROLE_ARN=arn:aws:iam::xaxsaxsadsa:role/databricks_nss
-        AWSSecretsManager build = AWSSecretsManagerClientBuilder.standard().withRegion("eu-west-1").build();
-        GetSecretValueResult foo = build.getSecretValue(new GetSecretValueRequest().withSecretId("foo"));
-        System.out.println(foo.getSecretString());
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> {
+            // run with env variable AWS_ROLE_ARN=arn:aws:iam::xaxsaxsadsa:role/databricks_nss
+            AWSSecretsManager build = AWSSecretsManagerClientBuilder.standard().withRegion("eu-west-1").build();
+            GetSecretValueResult foo = build.getSecretValue(new GetSecretValueRequest().withSecretId("foo"));
+            System.out.println(foo.getSecretString());
+        };
     }
 }
